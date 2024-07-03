b67d05862194a63fa595dbef777b433b6dc1c441
adbemnwy <adbemnwy@gmail.com>
2024-07-03 16:48:03 +0700

    ok

--- app/src/main/java/com/example/Nhom7/LOPADAPTER/PM_Adapter.java
+++ app/src/main/java/com/example/Nhom7/LOPADAPTER/PM_Adapter.java
@@ -119,7 +119,7 @@ public class PM_Adapter extends RecyclerView.Adapter<PM_Adapter.PhieuMuonhoder> 
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
                 builder.setTitle("Delete");
-                builder.setIcon(R.drawable.ic_dele);
+                builder.setIcon(R.drawable.logo_utt);
                 builder.setMessage("Bạn có muốn xóa không?");
                 builder.setCancelable(false);
                 builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
